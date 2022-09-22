c4fcc5976f350302cb399bbcd10bab892a989bfb
Le Van Hung <hunga1ok@gmail.com>
2022-09-22 19:42:41 +0700

    import meals app

---
+++ android/app/src/main/kotlin/com/huncorp/meal_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huncorp.meal_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/categories_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amber,
+          canvasColor: const Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+              bodyMedium: const TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              titleMedium: const TextStyle(
+                fontSize: 24,
+                fontFamily: 'Robotoondensed',
+              ),
+              headlineMedium: const TextStyle(
+                fontSize: 24,
+                fontFamily: 'Robotoondensed',
+                fontWeight: FontWeight.bold
+              ))),
+      home: const CategoriesScreen(),
+    );
+  }
+}
