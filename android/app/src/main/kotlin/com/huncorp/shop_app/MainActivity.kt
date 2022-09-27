096a2938f82e2ef02988faa9b3d258f7d4ea1e3a
Le Van Hung <hunga1ok@gmail.com>
2022-09-27 17:26:28 +0700

    Defining a Model, create Grid & Item Widget and styling & Theming the app

---
+++ android/app/src/main/kotlin/com/huncorp/shop_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.huncorp.shop_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem({
+    super.key,
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        footer: GridTileBar(
+          backgroundColor: Colors.black54,
+          leading: IconButton(
+            icon: const Icon(Icons.favorite),
+            onPressed: () {},
+            color: Theme.of(context).primaryColor,
+          ),
+          title: Text(
+            title,
+            textAlign: TextAlign.center,
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.shopping_cart),
+            color: Theme.of(context).primaryColor,
+            onPressed: () {},
+          ),
+        ),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
