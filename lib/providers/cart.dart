41400b485b02c5be594fb18855ebdd2eaba9ddad
Le Van Hung <hunga1ok@gmail.com>
2022-09-30 15:21:03 +0700

    Snackbars ans undoing 'Add to Cart' Action

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -64,6 +64,26 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+        productId,
+        (existingCardItem) => CartItem(
+          id: existingCardItem.id,
+          title: existingCardItem.title,
+          quantity: existingCardItem.quantity - 1,
+          price: existingCardItem.price,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
   void clear() {
     _items.clear();
     notifyListeners();
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -42,6 +42,17 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).primaryColor,
             onPressed: () {
               cart.addItem(product.id, product.price, product.title);
+              ScaffoldMessenger.of(context).hideCurrentSnackBar();
+              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                content: const Text('Added item to cart!'),
+                duration: const Duration(seconds: 2),
+                action: SnackBarAction(
+                  label: 'Undo',
+                  onPressed: () {
+                    cart.removeSingleItem(product.id);
+                  },
+                ),
+              ));
             },
           ),
         ),
